01f46aca804472a602a3833e85824d99fee77346
RCascaes <32971058+RCascaes@users.noreply.github.com>
2017-12-01 21:12:15 -0200

    Update menuIndex.php

--- motor/view/menuIndex.php
+++ motor/view/menuIndex.php
@@ -8,6 +8,12 @@
 <?php 
 	}
 	else {
+		if ($_SESSION["locadora"] == TRUE) {
+?>
+			[ <a href="">Filiais</a>
+			<a href="">Veículos</a> ]
+<?php		
+		}
 		echo " - Bem-vindo " . $_SESSION["user"] . " - ";
 ?>
 		[ <a href="control/logout.php">Logout</a> ]
