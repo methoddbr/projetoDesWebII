eb600a87afefd21fb0d96b4d2b6a378c9ccc7ba4
RCascaes <rogercascaes@hotmail.com>
2017-12-08 01:30:55 -0200

    RCascaes

--- Documentação/Banco/inserts.sql
+++ Documentação/Banco/inserts.sql
@@ -5,7 +5,7 @@ insert into Marca (descricao) values ('Ford');
 insert into Marca (descricao) values ('Renault');
 insert into Marca (descricao) values ('Volkswagen');
 
-/* Inserts TIPO */
+/* Inserts P/ TESTE */
 insert into cliente (nome, email, senha, telefone) values ('Roger', 'roger@a.com', '1234', '12345678');
 insert into pessoa_fisica (id_cliente, sobrenome, cpf, habilitacao, dt_nascimento, sexo) values ('1', 'Cascaes', '11111111', '12345678910', '14-04-1994', 'M');
 
